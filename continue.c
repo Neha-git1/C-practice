43cbc4bea04399407771c888f47b1252285277df
Neha-git1 <nehahemnath992@gmail.com>
2025-08-08 13:17:01 +0530

    Edited code

--- continue.c
+++ continue.c
@@ -2,5 +2,19 @@
 ./continue*/
 #include<stdio.h>
 int main(){
+     int i;
+
+    printf("Demonstrating 'continue' in a loop:\n");
+
+    for (i = 1; i <= 10; i++) {
+        if (i == 5) {
+            printf("Skipping number %d using continue.\n", i);
+            continue;  // Skip the rest of this iteration
+        }
+        printf("Number: %d\n", i);
+    }
+
+    printf("Loop finished.\n");
+    return 0;
 
 }

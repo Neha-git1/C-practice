82c13e1fbea76ed881b983522c6d4e98953d7638
Neha-git1 <nehahemnath992@gmail.com>
2025-08-09 17:01:56 +0530

    added C string examples with strlen, strcpy, strcmp

---
+++ string.c
@@ -0,0 +1,30 @@
+/*gcc string.c -o string
+./string*/
+#include<stdio.h>
+#include<string.h>
+int main(){
+    //strlen
+    char str[]="Neha";  //use double quotes for string
+    printf("the length of the string is: %u\n",strlen(str));
+    //strcpy
+    char src[]="neha";
+    char dest[20];   //dest[20] means we have 20 bytes available
+    strcpy(dest,src);
+    printf("%s\n", dest);
+    //strcmp
+    char a[] = "apple";
+    char b[] = "banana";
+
+    int result = strcmp(a, b);
+
+    if (result == 0)
+        printf("Strings are equal\n");
+    else if (result < 0)
+        printf("a comes before b\n");
+    else
+        printf("a comes after b\n");
+    
+}
+
+
+
